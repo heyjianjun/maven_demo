2bada62b1a57a8b7a9a925c9a089884d7a090023
heyjianjun <heyjianjun@163.com>
2018-02-08 21:40:01 +0800

    2018-2-8heyjianjun

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by heyjianjun on 2018/2/8.
+ */
+public class helloworld {
+    public static void main(String[] args){
+        Integer a = 10;
+        Integer b = 400;
+        Integer c = addNum(a,b);
+        System.out.printf(c.toString());
+    }
+    public static Integer addNum(Integer a,Integer b){
+        Integer c = a + b;
+        return  c;
+    }
+}
